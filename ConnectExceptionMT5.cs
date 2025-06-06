5aa7869c868f92a5b508113c1cd0f927f128208f
Tim <admin@mrpc.pro>
2025-06-07 01:26:52 +0400

    Init

---
+++ ConnectExceptionMT5.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MetaRPC.CSharpMT5;
+
+[Serializable]
+internal class ConnectExceptionMT5 : Exception
+{
+    public ConnectExceptionMT5()
+    {
+    }
+
+    public ConnectExceptionMT5(string? message) : base(message)
+    {
+    }
+
+    public ConnectExceptionMT5(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
